2cd0cdbeaf6dfd86ec5a573aff63cad1bfa49819
Przemysław K. Rekucki <prekucki@rcl.pl>
2020-10-13 12:01:26 +0200

    skip pre-commit hooks on gh-pages

--- gen.sh
+++ gen.sh
@@ -9,7 +9,7 @@ cat installer.sh > dist/as-provider
 sed 's/YA_INSTALLER_VARIANT=provider/YA_INSTALLER_VARIANT=requestor/' < installer.sh > dist/as-requestor
 
 git add dist/as-provider dist/as-requestor
-git commit -m "update"
+git commit --no-verify -m "update"
 
 #git subtree push -f --prefix dist origin gh-pages
 git push origin `git subtree split --prefix dist tmp`:gh-pages --force
