7cdee8c2f456b8c9cd3c96a692aa8acaf247ec9a
Piotr Chromiec <tworec@golem.network>
2020-11-17 18:12:19 +0100

    warning for using gen.sh + support for older git

--- gen.sh
+++ gen.sh
@@ -1,5 +1,14 @@
 #! /bin/bash
 
+read -rp "Consider useing manual GitHub action instead
+https://github.com/golemfactory/ya-installer/actions?query=workflow%3A%22Update+installer%22
+Are you sure you want to continue (yes/no)?
+" yn
+case $yn in
+    Yes|yes|YES) ;;
+    * ) exit;;
+esac
+
 SAVED_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 git branch -D tmp
@@ -7,9 +16,9 @@ git checkout -b tmp
 
 python3.8 gen.py || exit 1
 
-git add dist/as-provider dist/as-requestor dist/dev/as-provider dist/dev/as-requestor
-git commit --no-verify -m "update"
+cp setup-kvm.sh dist
+git add dist/as-provider dist/as-requestor dist/dev/as-provider dist/dev/as-requestor dist/setup-kvm.sh
+git commit --no-verify -m "updated from console"
 
-#git subtree push -f --prefix dist origin gh-pages
-git push origin `git subtree split --prefix dist tmp`:gh-pages --force
-git checkout $SAVED_BRANCH
+git push origin "$(git subtree split --prefix dist tmp)":gh-pages --force
+git checkout "${SAVED_BRANCH}"
